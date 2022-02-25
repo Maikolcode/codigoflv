67dbbe1df671f044af7c51e46afa1a1eb455c481
juanestebanperezm <60897881+juanestebanperezm@users.noreply.github.com>
2022-02-25 18:00:04 -0500

    Revert "Agregado tarjetas de retos y agregada navegacion a el componente"

--- frontend/src/components/Cabecera.js
+++ frontend/src/components/Cabecera.js
@@ -6,10 +6,8 @@ import React from "react";
 import Certificado from "./Certificado"
 import Cuenta from "./Cuenta"
 import Compite from "./Compite"
-
-import Preparate from "./preparate/Preparate";
-import Reto from "./Reto";
-
+import Login from "./Login";
+import Preparate from "./Preparate";
 
 //ESTILOS
 import "../styles/Cabecera.css";
@@ -52,7 +50,6 @@ function Cabecera() {
           <Route exact path="/preparate" element={<Preparate/>}/>
           <Route exact path="/certificado" element={<Certificado/>}/>
           <Route exact path="/compite" element={<Compite/>}/>
-          <Route exact path="/reto/:id" element={<Reto/>}/>
           <Route exact path="/cuenta" element={<Cuenta/>}/>
           <Route exact path="/login" element={<Login/>}/>
         </Routes>
--- frontend/src/components/Reto.js
+++ frontend/src/components/Reto.js
@@ -1,29 +1,9 @@
-import React, {useState, useEffect} from "react";
-import { useParams } from "react-router-dom";
+import React from 'react'
 
 function Reto() {
-  const { id } = useParams();
-  const [{ description, title, language, dificultad }, setData] = useState([]);
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-  const getData = async () => {
-    const URL = `http://localhost:3000/challenges/retos/${id}`;
-    const data = await fetch(URL);
-    const reto = await data.json();
-    setData(reto);
-  };
-
   return (
-    <div>
-      <h1>{title}</h1>
-      <h1>{description}</h1>
-      <h1>{language}</h1>
-      <h1>{dificultad}</h1>
-    </div>
-  );
+    <div>Reto</div>
+  )
 }
 
-export default Reto;
+export default Reto
